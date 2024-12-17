e388a0d22180fbe032738757991d969d4279e607
Ilan Lallemand <ilan.lallemand91@gmail.com>
2024-12-17 12:22:55 +0100

    [FEATURE] CRUD product admin dashboard

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,2 +1,12 @@
+
+// composant client
+
 export { default as NavbarAuth } from './NavbarAuth/NavbarAuth';
 export { default as SidebarAuthentification } from './SidebarAuth/SidebarAuth';
+
+
+
+// composant administrateur
+
+export { default as AdminProductList } from './admin/AdminProductList/AdminProductList'
+export { default as AdminCreateProductForm } from './admin/AdminCreateProductForm/AdminCreateProductForm'
