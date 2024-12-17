ed7aa0345f04878e47c736fc2386d27a61835b5e
Kaowarstail <victor.dane92@gmail.com>
2024-12-17 14:05:06 +0100

    Revert "[FEATURE] CRUD product admin dashboard"

--- src/app/hooks/useCreateAdminProducts.tsx
+++
@@ -1,9 +0,0 @@
-export async function createAdminProduct(productData: any) {
-  const response = await fetch("/api/admin/products", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(productData),
-  });
-
-  return await response.json();
-}
